05c97e15c0096cc2e33b60a4a311df413933da3f
EasonChen <ychen@wustl.edu>
2018-04-19 20:38:12 -0500

    milestone2

---
+++ milestone_2/ml/gp.py
@@ -0,0 +1,114 @@
+import sys
+sys.path.insert(0, '/Users/op')
+
+import numpy as np
+import scipy.spatial.distance as sp
+import preprocessing
+
+# computing covariance matrix for GP by means of SE kernel:
+# depending on what Xs and Ys are, this can be K, K*, or K**.
+# scipy.spatial.distance.pdist and .cdist are used to compute
+# the matrix of squared Euclidian distances efficiently.
+# "lsquared" is an argument, sigmaF is assumed to be 1.
+
+def compute_se_kernel_matrix(Xs, Ys, lsquared):
+    if Xs is Ys: dist = sp.squareform(sp.pdist(Xs, "sqeuclidean"))
+    else: dist = sp.cdist(Xs, Ys, "sqeuclidean")
+    K = np.exp(-dist / (2.0*lsquared))
+    return K
+
+# def compute_se_kernel_matrix(Xs, Ys, lsquared):
+#     return np.dot(Xs, np.transpose(Ys))
+
+# actual Gaussian Process: the target values for unobserved data
+# X* ("X_s") are inferred based on the observed dataset "X" and the
+# corresponding labels "T". "lsquared" is used while computing the
+# SE-kernel function. sigmaF and sigmaN are taken as 1 and 0, as
+# only mean, and not variance, is of practical importance here.
+def gp(X, T, X_s, lsquared):
+    # computing the K matrix for the observed data
+    K = compute_se_kernel_matrix(X, X, lsquared)
+    print "K matrix computed"
+
+    # computing the K* transposed matrix
+    K_s_T = compute_se_kernel_matrix(X_s, X, lsquared)
+    print "K*^T matrix computed"
+
+    # inverting the K matrix
+    K_inv = np.linalg.inv(K)
+    print "K matrix inverted"
+
+    # multiplying the K*^T and K^-1 matrices
+    K_s_T_times_K_inv = np.dot(K_s_T, K_inv)
+    print "K*^T times K^-1 matrix computed"
+
+    size, classes = len(X_s), T.max()+1
+    predictions = np.zeros((size, classes))
+
+    # for each class k:
+    for k in range(classes):
+        # transforming target labels into k-class vs. rest
+        # representation: 1.0 for the k-class, -1.0 for rest
+        k_class_vs_rest = np.where(T == k, 1.0, -1.0)
+
+        # inferring the corresponding k-class (1.0) vs. the rest (-1.0) values
+        # for the unobserved data by multiplying pre-computed [K*^T times K^-1]
+        # matrix by the above "k_class_vs_rest" vector. what is obtained is the
+        # set of the mean values of the k-class vs. rest regression in the
+        # unobserved data points X* ("X_s")
+        result = np.dot(K_s_T_times_K_inv, k_class_vs_rest)
+
+        # storing the predicted k-class vs.
+        # rest means in the data points X*
+        predictions[:, k] = result
+
+    print "{0} binary classifications done".format(classes)
+
+    # inferring actual target labels in accordance with
+    # the highest predicted k-class vs. rest mean
+    labels = np.argmax(predictions, axis=1)
+    print "Class labels detected"
+    print
+
+    return labels
+
+
+# comparing predicted vs. true labels
+# and returning the corresponding error score
+def get_error_score(T_predicted, T_true):
+    count = np.count_nonzero(np.array(T_predicted != T_true))
+    return count * 100.0 / len(T_predicted)
+
+
+if __name__ == "__main__":
+
+    # command-line arguments (and their default values)
+
+    ntrain = 1000      # number of training samples used
+    ntest =  600       # number of testing samples used
+
+    deskew = True       # deskew input images or not (by default: yes)
+    normalize = True    # normalize input vectors or not (by default: yes)
+
+    lsquared = 1.0     # l^2 used in SE kernel computation
+
+    # processing command-line arguments
+    print "Gaussian Processes"
+    print
+
+    print "Reading data..."
+    # reading the data and applying configured pre-processing steps
+    X_train, T_train = preprocessing.get_training_data(ntrain, normalize=normalize, deskew=deskew)
+    X_test, T_test = preprocessing.get_testing_data(ntest, normalize=normalize, deskew=deskew)
+    print "{0} training data read".format(len(X_train))
+    print "{0} testing data read".format(len(X_test))
+    print
+
+    # running a Gaussian process on training and testing sets, with "lsquared"
+    T_predicted = gp(X_train, T_train, X_test, lsquared=1)
+
+    # evaluating the model performance on the testing set
+    print "Testing Set Error: {0:.3f}".format(
+        get_error_score(T_predicted, T_test)
+    )
+    print
